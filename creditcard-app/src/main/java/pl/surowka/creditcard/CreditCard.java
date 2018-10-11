9c8b1cd1c97df1d1c5da641a513a1c76fdf17821
Sebastian Surowka <sebastian.surowka@gmail.com>
2018-10-11 10:28:13 +0000

    add methods

---
+++ creditcard-app/src/main/java/pl/surowka/creditcard/CreditCard.java
@@ -0,0 +1,15 @@
+package pl.surowka.creditcard;
+
+class CreditCard{
+    
+    public void assignLimit(double money){
+        
+    }
+    
+    public void getLimit(){
+        
+        return 2000;
+    }
+    
+    
+}
---
+++ creditcard-app/src/test/java/pl/surowka/creditcard/CreditCardTest.java
@@ -0,0 +1,17 @@
+package pl.surowka.creditcard;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CreditCardTest{
+    
+    
+    @Test
+    public void allowAssignLimitToCard(){
+        CreditCard card = new CreditCard();
+        
+        card.assignLimit(2000);
+        
+        Assert.assertTrue(card.getLimit() == 2000);
+    }
+}
